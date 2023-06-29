53878dd2972b1c4eb6ba25ca10427576455fa47e
Victor9530 <garrotevicgmail.com>
2023-06-29 10:39:13 -0300

    correccion 29/09/2023

--- conec_sql.py
+++ conec_sql.py
@@ -4,9 +4,10 @@ def connection():
 
     try:   
        connSqlServer = pyodbc.connect(driver='{ODBC Driver 17 for SQL server}',
-                               server='tcp:miservidorsql9530.database.windows.net',
-                               database='Alquileres',
-                               uid='VictHugo',pwd='Victor9530')
+                               server='alquileres.mssql.somee.com',
+                               database='alquileres',
+                               uid='mrnct_SQLLogin_1',
+                               pwd='81x55n8d5y')
        
     except:
         print ('coneccion cerrada')
